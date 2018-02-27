b11194d8806f1c1182094a90856814750aef1ca3
A. Vicki Crosson <amber.crosson@nytimes.com>
2018-02-26 19:28:00 -0500

    Added new urls/views for additional tabs

--- wedding/urls.py
+++ wedding/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^when/$', views.when, name='when'),
+    url(r'^where/$', views.where, name='where'),
+    url(r'^why/$', views.why, name='why'),
+    url(r'^how/$', views.how, name='how')
 ]
--- wedding/views.py
+++ wedding/views.py
@@ -6,3 +6,15 @@ def home(request):
     return render(request, 'home.html', context={
         'save_the_dates': SAVE_THE_DATE_CONTEXT_MAP
     })
+
+def when(request):
+    return render(request, 'when.html')
+
+def where(request):
+    return render(request, 'where.html')
+
+def why(request):
+    return render(request, 'why.html')
+
+def how(request):
+    return render(request, 'how.html')
