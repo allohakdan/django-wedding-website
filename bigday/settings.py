61b031e7c5f9c2fa502776d760f065363a99a60c
dan brooks <dan@danbrooks.net>
2018-04-15 12:04:06 -0400

    email is working

--- bigday/settings.py
+++ bigday/settings.py
@@ -118,8 +118,10 @@ USE_L10N = True
 USE_TZ = True
 
 # [db] Email settings
+# TO SEND EMAIL - YOU MUST SET THE PASSWORD AS AN ENVIORNMENT VARIABLE
+# $ export GMAILPASS="verysecretpassword"
 import os
-EMAIL_BACKEND = 'django_smtp_ssl.SSLEmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST= 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT=587
