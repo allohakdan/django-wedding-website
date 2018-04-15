e283dbc1baf8bc5c1a45ad86f7aa2ab21a149762
dan brooks <dan@danbrooks.net>
2018-04-15 11:50:14 -0400

    takes a stab at email settings

--- bigday/settings.py
+++ bigday/settings.py
@@ -117,6 +117,17 @@ USE_L10N = True
 
 USE_TZ = True
 
+# [db] Email settings
+import os
+EMAIL_BACKEND = 'django_smtp_ssl.SSLEmailBackend'
+EMAIL_HOST= 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT=587
+EMAIL_HOST_USER="crosson.brooks@gmail.com"
+EMAIL_HOST_PASSWORD=str(os.environ['GMAILPASS'])
+DEFAULT_EMAIL_FROM = "crosson.brooks@gmail.com"
+
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
