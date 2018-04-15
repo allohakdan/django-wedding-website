006a6019248a01757034fd9cb5f1f13b71922176
A. Vicki Crosson <amber.crosson@nytimes.com>
2018-04-15 11:51:16 -0400

    Updated guest handling

--- guests/management/commands/import_guests.py
+++ guests/management/commands/import_guests.py
@@ -3,7 +3,10 @@ from guests import csv_import
 
 
 class Command(BaseCommand):
-    args = 'filename'
+    #args = 'filename'
+
+    def add_arguments(self, parser):
+        parser.add_argument('filename')
 
     def handle(self, filename, *args, **kwargs):
         csv_import.import_guests(filename)
